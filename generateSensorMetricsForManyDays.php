2794fea0ca53ef4247693835b6bd4fa609ab39f4
Jeff Rieger <jeff.rieger@digi.com>
2019-03-25 15:08:45 -0400

    Added ability to pass location id from the cli

--- generateSensorMetricsForManyDays.php
+++ generateSensorMetricsForManyDays.php
@@ -13,8 +13,16 @@ $dbMaster = new PDO('mysql:host=172.21.2.20;dbname=tempmon;charset=utf8', 'root'
 $dbMaster->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
+
+if(isset($argc) && $argc == 2) {
+  $loc_query_string = "S.location = " . (int)$argv[1];
+} else {
+  $loc_query_string = "S.location > 0";
+}
+
 //Only check for tasks marked null.
-$sth = $dbMaster->prepare("SELECT S.id AS sensorID,L.id AS locationID, S.target AS equipmentID,L.timezone,T.lower_limit, T.upper_limit,S.tx_interval,L.name AS locationName FROM sensor S LEFT JOIN location L ON L.id = S.location LEFT JOIN target T ON T.id = S.target WHERE T.deleted IS NULL AND S.deleted IS NULL AND L.deleted IS NULL AND S.target IS NOT NULL AND S.location > 0");
+$sth = $dbMaster->prepare("SELECT S.id AS sensorID,L.id AS locationID, S.target AS equipmentID,L.timezone,T.lower_limit, T.upper_limit,S.tx_interval,L.name AS locationName FROM sensor S LEFT JOIN location L ON L.id = S.location LEFT JOIN target T ON T.id = S.target WHERE T.deleted IS NULL AND S.deleted IS NULL AND L.deleted IS NULL AND S.target IS NOT NULL AND " . $loc_query_string);
+
 
 $sth->execute();
 
@@ -22,9 +30,13 @@ $numberOfDaysToCalculateMetrics = 10;
 
 while ($row = $sth->fetchObject()) {
 
-  for($days = 2; $days <= $numberOfDaysToCalculateMetrics; $days++) {
+  echo "Generating metrics for location: " . $row->locationName . "\n";
+
+  for($days = 1; $days <= $numberOfDaysToCalculateMetrics; $days++) {
 
     $Day = getYesterdayAtLocationByTimeZone($row->timezone, 'Y-m-d', $days);
+
+    echo $row->sensorID . "-" . $Day . " (completed) \n";
 
     $SensorSummaryByDay = new SensorSummaryByDayClass($row->locationID, $row->sensorID, $row->equipmentID, $Day, $row->timezone, $row->lower_limit, $row->upper_limit, $row->tx_interval);
 
